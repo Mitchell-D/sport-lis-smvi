af265e9b78b1906dcb1a6328e9ad4794240c5284
Mitchell-D <mtd0012@uah.edu>
2025-12-15 14:40:32 -0600

    added demo of different moving average techniques

--- demo_moving_window.py
+++ demo_moving_window.py
@@ -1,13 +1,19 @@
+""" Comparison of different approaches for calculating moving averages. """
 import numpy as np
 
-x = np.random.random_sample((336,32,32))
+size = 1024
 window = 5
+x = np.random.random_sample(size)
 
-## method 1
+## method 1: final size is size - window
+## Since the first element of the result negates the first element of the
+## input array, the first input element isn't explicitly used in an average.
 cs = np.cumsum(x, axis=0)
 ma1 = (cs[window:]-cs[:-window]) / window
 
-## method 2
+## method 2: final size is size - window + 1
+## each element is mean of previous window-1 elements and itself, so there are
+## no elements in the input array that are not explicitly used in an average.
 flt = np.ones(window) / window
 ma2 = np.apply_along_axis(
         lambda m:np.convolve(m,flt,mode="valid"),
@@ -15,7 +21,7 @@ ma2 = np.apply_along_axis(
         arr=x
         )
 
-## method 3
+## method 3, showing that one fewer element is needed for the same result.
 ma3 = np.apply_along_axis(
         lambda m:np.convolve(m,flt,mode="valid"),
         axis=0,
